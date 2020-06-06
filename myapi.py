04bc24bb454ad92fb9fb23f80eee34ec601b04df
handlake <ccteacher@naver.com>
2020-06-06 16:07:22 +0900

    second commit

--- myapi.py
+++ myapi.py
@@ -1,4 +1,4 @@
-def isosu(n):
+def issosu(n):
     if n<2:
         return False
     if not n%2:
@@ -7,3 +7,12 @@ def isosu(n):
         if not n%i:
             return False
     return True
+def sosulist(n):
+    return [a for a in range(2, n+1) if issosu(a)]
+def divisor(n):
+    rtn = []
+    for a in range(1, n//2+1):
+        if not n%a:
+            rtn.append(a)
+    rtn.append(n)
+    return rtn
---
+++ test.py
@@ -0,0 +1,6 @@
+import myapi
+
+soli = myapi.sosulist(10)
+print(soli)
+dili = myapi.divisor(10)
+print(dili)
