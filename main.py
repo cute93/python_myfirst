9849e71565601afc0e973a0e7a1b02e28862837f
Handlake_Wizard <kyungheesillang@gmail.com>
2020-06-09 13:13:19 +0900

    downimg

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import header
-import numheader as nh 
+import requests
+from bs4 import BeautifulSoup
+import os
 
-li = header.randlist(6, 45)
-print(li)
-li = nh.sosulist(100)
-print(li)
+inp = input('이미지 : ')
+url = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='+inp
+
+res = requests.get(url)
+soup = BeautifulSoup(res.content, 'html.parser')
+imglist = soup.find_all('a', class_='thumb _thumb')
+if len(imglist) > 0:
+    os.mkdir(inp)
+    os.chdir(inp)
+n=1
+for img in imglist:
+    i = img.find('img')
+    imgsource = i.attrs['data-source']
+    imgdata = requests.get(imgsource)
+    if imgdata.ok == True:
+        with open(inp+str(n)+'.jpg', 'wb') as f:
+            f.write(imgdata.content)
+            n+=1
+
+    
